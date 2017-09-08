6ebd248b1455a8342c405e9e06dd2ee0f93da96c
Sergey S <sery.sht@gmail.com>
2017-09-09 02:49:01 +0300

    moved mp manipulations to separate module runtask.py

---
+++ TrashExplorer/runtask.py
@@ -0,0 +1,53 @@
+from smrm import trash
+import multiprocessing
+from django.shortcuts import get_object_or_404
+from TrashExplorer.models import TrashInfo, TaskInfo
+
+lock = multiprocessing.Lock()
+
+
+def run_task(task_id):
+
+    task_obj = get_object_or_404(TaskInfo, id=task_id)
+    task_obj.is_busy = True
+    trash_obj = get_object_or_404(TrashInfo, id=task_obj.trash.id)
+    trash_obj.is_busy = True
+
+    with lock:
+        trash_obj.save()
+        task_obj.save()
+
+    task_dict = task_obj.__dict__
+    trash_dict = {}
+    trash_dict.update(trash_obj.__dict__)
+    trash_dict.update(task_dict)
+
+    t = trash.Trash(task_obj.trash.trash_path,
+                    storage_time=trash_dict["file_storage_time"],
+                    trash_maximum_size=trash_dict["trash_maximum_size"],
+                    log_path=trash_dict["log_path"],
+                    dry_run=trash_dict["dry_run"],
+                    force=trash_dict["force"],
+                    verbose=task_dict["verbose"]
+                    )
+
+    if task_obj.operation_type == "simple delete":
+        targets = task_obj.target.split()
+        return_list = t.delete_to_trash(targets)
+        for info_message, _ in return_list:
+            task_obj.info_message += "\n" + info_message
+    else:
+        if task_obj.regex != "":
+            return_list = t.delete_to_trash_by_reg(task_obj.regex, task_obj.target)
+            for info_message, _ in return_list:
+                task_obj.info_message += "\n" + info_message
+        else:
+            task_obj.info_message = "You didn't enter regex"
+
+    task_obj.done = True
+    task_obj.is_busy = False
+    trash_obj.is_busy = False
+
+    with lock:
+        trash_obj.save()
+        task_obj.save()
--- TrashExplorer/views.py
+++ TrashExplorer/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
-from .models import TrashInfo, TaskInfo
-from smrm import trash, trashconfig
-from .forms import TrashForm, TaskForm
+from TrashExplorer.models import TrashInfo, TaskInfo
+from smrm import trash
+from TrashExplorer.forms import TrashForm, TaskForm
 from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 import multiprocessing
 import os
+from runtask import run_task
 
 
 class TrashList(ListView):
@@ -123,57 +124,10 @@ def delete_task(request, task_id):
     task_object.delete()
     return redirect('/task_list')
 
-lock = multiprocessing.Lock()
-
-
-def run_mp(task_obj, trash_obj):
-    task_dict = task_obj.__dict__
-    trash_dict = {}
-    trash_dict.update(trash_obj.__dict__)
-    trash_dict.update(task_dict)
-
-    t = trash.Trash(task_obj.trash.trash_path,
-                    storage_time=trash_dict["file_storage_time"],
-                    trash_maximum_size=trash_dict["trash_maximum_size"],
-                    log_path=trash_dict["log_path"],
-                    dry_run=trash_dict["dry_run"],
-                    force=trash_dict["force"],
-                    verbose=task_dict["verbose"]
-                    )
-
-    if task_obj.operation_type == "simple delete":
-        targets = task_obj.target.split()
-        return_list = t.delete_to_trash(targets)
-        for info_message, _ in return_list:
-            task_obj.info_message += "\n" + info_message
-    else:
-        if task_obj.regex != "":
-            return_list = t.delete_to_trash_by_reg(task_obj.regex, task_obj.target)
-            for info_message, _ in return_list:
-                task_obj.info_message += "\n" + info_message
-        else:
-            task_obj.info_message = "You didn't enter regex"
-
-    task_obj.done = True
-    task_obj.is_busy = False
-    trash_obj.is_busy = False
-
-    with lock:
-        trash_obj.save()
-        task_obj.save()
-
 
 def run(request, task_id):
-    task_obj = get_object_or_404(TaskInfo, id=task_id)
-    task_obj.is_busy = True
-    trash_obj = get_object_or_404(TrashInfo, id=task_obj.trash.id)
-    trash_obj.is_busy = True
 
-    with lock:
-        trash_obj.save()
-        task_obj.save()
-
-    p = multiprocessing.Process(target=run_mp, args=(task_obj, trash_obj))
+    p = multiprocessing.Process(target=run_task, args=(task_id, ))
     p.start()
 
     return redirect('/task_list')
